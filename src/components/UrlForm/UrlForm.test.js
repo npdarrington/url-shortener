5b4bd4ac0da4a04b4146e2deb6a95a9a5ee0cf75
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 10:22:17 -0700

    Test input should be able to type into input boxes

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -16,4 +16,14 @@ describe('UrlForm', () => {
     expect(screen.getByPlaceholderText('URL to Shorten...')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Shorten Please!'})).toBeInTheDocument();
   })
+
+  test('should let user type into the input boxes', () => {
+    const titleInput = screen.getByPlaceholderText('Title...');
+    const urlInput = screen.getByPlaceholderText('URL to Shorten...');
+    userEvent.type(titleInput, 'Testing 1');
+    userEvent.type(urlInput, 'https://unsplash.com/photos/1p7TrM0LkXc');
+    expect(titleInput.value).toBe('Testing 1');
+    expect(urlInput.value).toBe('https://unsplash.com/photos/1p7TrM0LkXc');
+  })
+  
 })
