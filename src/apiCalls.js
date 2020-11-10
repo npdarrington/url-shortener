c3496fc581af06e2f873ac94b59c3a714fcaf694
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 09:28:35 -0700

    Added postUrl method and exported

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (initObject) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: initObject
+  })
+    .then(response => response.json());
+}
