d41305564047562afefabc515bee6277773be86d
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 11:15:41 -0700

    Added button that onClick calls deleteUrl method passed from props with url.id as arg

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -8,6 +8,7 @@ const UrlContainer = props => {
         <h3>{url.title}</h3>
         <a href={url.short_url} target="blank">{url.short_url}</a>
         <p>{url.long_url}</p>
+        <button onClick={() => props.deleteUrl(url.id)}>Delete</button>
       </div>
     )
   });
