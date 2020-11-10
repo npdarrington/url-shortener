c46dd98bacbe54c58b5233600ccf9b3876a12c17
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 10:27:40 -0700

    Import statements needed to test App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
+import App from './App';
