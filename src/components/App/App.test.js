7191be1bda7e713ce2b4f1e075abb593805c4344
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 10:38:44 -0700

    Test App to render all urls found from fetch call on page

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { getUrls, postUrl } from '../../apiCalls'
@@ -29,6 +29,18 @@ describe('App', () => {
     getUrls.mockResolvedValue(mockUrlData);
     render(<App />);
     expect(screen.getByRole('heading', { name: 'URL Shortener'})).toBeInTheDocument();
+  })
+  
+  test('should render any urls found in fetch call to DOM', async () => {
+    getUrls.mockResolvedValue(mockUrlData);
+    render(<App />);
+    await waitFor(() => screen.getByRole('heading', { name: 'URL Shortener'}))
+    expect(screen.getByText('Testing 1')).toBeInTheDocument();
+    expect(screen.getByText('https://unsplash.com/photos/1p7TrM0LkXc')).toBeInTheDocument();
+    expect(screen.getByText('http://localhost:3001/useshorturl/1')).toBeInTheDocument();
+    expect(screen.getByText('Testing 2')).toBeInTheDocument();
+    expect(screen.getByText('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')).toBeInTheDocument();
+    expect(screen.getByText('http://localhost:3001/useshorturl/2')).toBeInTheDocument();
   })
   
 })
