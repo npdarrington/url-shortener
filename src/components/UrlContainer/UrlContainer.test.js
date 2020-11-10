e4cc4bfb8452fd5f7ca8a722d9a23a9dc3fc09ad
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 11:43:58 -0700

    Test should delete a url when delete button is clicked

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import UrlContainer from './UrlContainer';
@@ -20,7 +21,8 @@ describe('UrlContainer', () => {
         title: 'Testing 2'
       }
     ]
-    render(<UrlContainer urls={mockUrls} />)
+    const mockDeleteUrl = jest.fn();
+    render(<UrlContainer urls={mockUrls} deleteUrl={mockDeleteUrl} />)
     expect(screen.getByText('Testing 1')).toBeInTheDocument();
     expect(screen.getByText('https://unsplash.com/photos/1p7TrM0LkXc')).toBeInTheDocument();
     expect(screen.getByText('http://localhost:3001/useshorturl/1')).toBeInTheDocument();
@@ -30,8 +32,31 @@ describe('UrlContainer', () => {
   })
 
   test('should render a conditional message if no urls are found', () => {
-    render(<UrlContainer urls={[]} />);
+    const mockDeleteUrl = jest.fn();
+    render(<UrlContainer urls={[]} deleteUrl={mockDeleteUrl} />);
     expect(screen.getByText('No urls yet! Find some to shorten!')).toBeInTheDocument();
   })
-  
+
+  test('should delete a url when delete button is clicked', () => {
+    const mockUrls = [
+      {
+        id: 1,
+        long_url: 'https://unsplash.com/photos/1p7TrM0LkXc',
+        short_url: 'http://localhost:3001/useshorturl/1',
+        title: 'Testing 1'
+      },
+      {
+        id: 2,
+        long_url: 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+        short_url: 'http://localhost:3001/useshorturl/2',
+        title: 'Testing 2'
+      }
+    ]
+    const mockDeleteUrl = jest.fn();
+    render(<UrlContainer urls={mockUrls} deleteUrl={mockDeleteUrl} />)
+    const getAllDeleteBtn = screen.getAllByRole('button', { name: 'Delete'});
+    userEvent.click(getAllDeleteBtn[1]);
+    expect(mockDeleteUrl).toHaveBeenCalledTimes(1);
+    expect(mockDeleteUrl).toHaveBeenCalledWith(2);
+  })
 })
