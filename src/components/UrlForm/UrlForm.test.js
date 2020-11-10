80194710b928c086ef398723ac3bd86a972c8c18
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 10:16:01 -0700

    Setup imports needed to test UrlForm

---
+++ src/components/UrlForm/UrlForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import UrlForm from './UrlForm';
+
