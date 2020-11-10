9aac953331171cef4af9c800d0c5c075933deac7
Nathan Darrington <npdarrington@icloud.com>
2020-11-10 10:13:42 -0700

    Test should render all Urls to the page

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -4,3 +4,28 @@ import '@testing-library/jest-dom';
 
 import UrlContainer from './UrlContainer';
 
+describe('UrlContainer', () => {
+  test('should render all Urls to the page', () => {
+    const mockUrls = [
+      {
+        id: 1,
+        long_url: 'https://unsplash.com/photos/1p7TrM0LkXc',
+        short_url: 'http://localhost:3001/useshorturl/1',
+        title: 'Testing 1'
+      },
+      {
+        id: 2,
+        long_url: 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
+        short_url: 'http://localhost:3001/useshorturl/2',
+        title: 'Testing 2'
+      }
+    ]
+    render(<UrlContainer urls={mockUrls} />)
+    expect(screen.getByText('Testing 1')).toBeInTheDocument();
+    expect(screen.getByText('https://unsplash.com/photos/1p7TrM0LkXc')).toBeInTheDocument();
+    expect(screen.getByText('http://localhost:3001/useshorturl/1')).toBeInTheDocument();
+    expect(screen.getByText('Testing 2')).toBeInTheDocument();
+    expect(screen.getByText('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')).toBeInTheDocument();
+    expect(screen.getByText('http://localhost:3001/useshorturl/2')).toBeInTheDocument();
+  })
+})
